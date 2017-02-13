3ed4393086986ce87f1fb4e33d57bbbc2c72b89f
Adrianna Valdivia <adrianna.valdivia@gmail.com>
2017-02-13 15:37:47 -0500

    recipe and shopping list services added to view ingredients in each recipe

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import {Recipe} from "./recipe";
+import {Ingredient} from "../shared/ingredient";
 
 @Injectable()
 export class RecipeService {
 
   private recipes: Recipe[] = [
-    new Recipe('Dummy1', 'Dummy1', 'http://www.terameracake.com/wp-content/uploads/2015/05/Dummy-Couple-Small-size.jpg', [] ),
+    new Recipe('Dummy1', 'Dummy1', 'http://www.terameracake.com/wp-content/uploads/2015/05/Dummy-Couple-Small-size.jpg', [
+      new Ingredient ( 'French Fries', 2),
+      new Ingredient ( 'Pork Meat', 1)
+    ] ),
     new Recipe('Dummy2', 'Dummy2', 'http://www.terameracake.com/wp-content/uploads/2015/05/Dummy-Couple-Small-size.jpg', [] )
   ];
 
